4ab9701b49768900e22be60e06b9ebb66338419d
mariem-crypto <mariem.jouili@polytechnicien.tn>
2026-07-03 15:57:40 +0100

    Add : Add Api

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,39 +1,55 @@
 class ApiConstants {
   ApiConstants._();
 
-  // Base API
+  /// Base URL
   static const String baseUrl = 'https://api.valomnia.com/api/';
 
-  // Tenant URL
+  /// Tenant Base URL
   static const String tenantBaseUrl = 'https://api.valomnia.com/api/';
 
-  // Headers
+  /// Headers
   static const String jsonContentType = 'application/json';
   static const String formUrlEncodedContentType =
       'application/x-www-form-urlencoded';
 
+  // ==========================
   // Authentication
+  // ==========================
+
   static const String login = 'loginB2B';
-  static const String checkPassword = '/check-password';
-  static const String updateUser = '/update-user';
-  static const String forgotPassword = '/forgot-password';
-  static const String resetPassword = '/reset-password';
+  static const String checkPassword = 'check-password';
+  static const String updateUser = 'update-user';
+  static const String userCheck = 'user/check';
+  static const String forgotPassword = 'forgot-password';
+  static const String resetPassword = 'reset-password';
 
+  // ==========================
   // Catalog
-  static const String items = '/items';
-  static const String itemCategories = '/itemCategories';
-  static const String prices = '/prices';
-  static const String item = '/item';
+  // ==========================
 
+  static const String items = 'items';
+  static const String itemCategories = 'itemCategories';
+  static const String prices = 'prices';
+  static const String item = 'item';
+
+  // ==========================
   // Customer
-  static const String customer = '/customer';
-  static const String customerMinOrderTotal = '/customerMinOrderTotal';
+  // ==========================
 
+  static const String customer = 'customer';
+  static const String customerMinOrderTotal = 'customerMinOrderTotal';
+
+  // ==========================
   // Orders
-  static const String orders = '/orders';
-  static const String addOrder = '/add-order';
-  static const String checkoutEmail = '/checkoutEmail';
+  // ==========================
 
+  static const String orders = 'orders';
+  static const String addOrder = 'add-order';
+  static const String checkoutEmail = 'checkoutEmail';
+
+  // ==========================
   // System
-  static const String health = '/user';
+  // ==========================
+
+  static const String health = 'user';
 }
